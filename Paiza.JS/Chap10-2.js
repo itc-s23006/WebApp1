9c5c4ed5627fdda434244fc2acb6521a92d4ff91
itc-s23006 <s23006@std.it-college.ac.jp>
2023-12-12 02:55:53 +0000

    条件に一致したら処理を実行する

---
+++ Paiza.JS/Chap10-2.js
@@ -0,0 +1,20 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('整数を入力してください: ', input => {
+  const number = parseInt(input)
+
+  if (!isNaN(number)) {
+    if (number < 100) {
+      console.log(`${number}`)
+    }
+  } else {
+    console.log('入力が無効です。整数を入力してください。')
+  }
+
+  rl.close()
+})
