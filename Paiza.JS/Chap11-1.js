3b9ca6d3943b7bec98706e766ff16e5887466e13
s23006 <s23006@std.it-college.ac.jp>
2023-12-13 03:12:16 +0000

    Chap11-1.js

---
+++ Paiza.JS/Chap11-1.js
@@ -0,0 +1,27 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const lines = []
+
+// 標準入力から複数行の文字列を受け取る
+rl.on('line', line => {
+  lines.push(line)
+})
+
+// 標準入力が終了したときの処理
+rl.on('close', () => {
+  // lines配列の各要素に対して処理を行う
+  for (const inputString of lines) {
+    // 入力が "Hello" と等しい場合の処理
+    if (inputString === 'Hello') {
+      console.log('こんにちは')
+    } else {
+      // 入力が "Hello" と等しくない場合の処理
+      console.log(`${inputString}はHelloではない`)
+    }
+  }
+})
